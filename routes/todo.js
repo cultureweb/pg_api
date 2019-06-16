64b539c171914f77119c1e11256c2a9d045fcb78
ced <cedric.milius@yahoo.fr>
2019-06-16 22:43:04 +0200

    render all rows in localhost:3000/todo

--- routes/todo.js
+++ routes/todo.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TODO' });
+const knex = require('../db/knex');
+
+/* This router is mounted at http:localhost:3000/todo */
+router.get('/', function (req, res, next) {
+    knex('todo')
+    .select()
+    .then(todos => {
+        res.render('all', { todos: todos });
+
+    });
 });
 
 module.exports = router;
